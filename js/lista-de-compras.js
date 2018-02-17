62a1962ba138651aecc91f71c7515c8f34db1734
ISDI <isdi@ISDIs-MacBook-Air.local>
2018-02-17 14:11:38 -0600

    Animación del botton

--- js/lista-de-compras.js
+++ js/lista-de-compras.js
@@ -25,7 +25,11 @@ function escribeLista(){
   var elemento = prompt("ingresa el nuevo ingrediente");
   if( elemento ){
     shopping.push( elemento );
+    renderizaLista();
   }
+}
+function renderizaLista(){
+
   var indice = 0;
   var indice_maximo = shopping.length;
 document.getElementById('list').innerHTML = "";
@@ -36,14 +40,31 @@ document.getElementById('list').innerHTML = "";
 //document.getElementById('list').innerHTML =
         //document.getElementById('list').innerHTML + "<li>" + shopping[ indice ]+ "</li>";
 
-//$('#list').append( "<li>" + shopping[ indice ]+ "</li>");
-var li = document.createElement('li');
-var text = document.createTextNode(shopping[indice]);
-li.appendChild(text);
-document.getElementById('list').appendChilid( li );
+$('#list').append( "<li class='list-group-item'>" + shopping[ indice ]+
+"<button class='btn btn-danger float-right eliminar btn-sm'><i class='fas fa-times-circle'></i></button>" +
+"</li>");
+//var li = document.createElement('li');
+//var text = document.createTextNode(shopping[indice]);
+//li.appendChild(text);
+//document.getElementById('list').appendChilid( li );
   indice++;
   }
 }
 
+$(document).ready(function(){
+$('#list').delegate('button.eliminar','mouseenter',function( event ){
+  event.preventDefault();
+  var indice = $(this).parent().index();
+  $(this).parent().addClass('resaltado');
+  //alert( indice );
+})
+$('#list').delegate('button.eliminar','mouseleave',function( event ){
+  event.preventDefault();
+  var indice = $(this).parent().index();
+  $(this).parent().removeClass('resaltado');
+  //alert( indice );
+})
+})
+
 document.getElementById('agregar').addEventListener('click', escribeLista);
-escribeLista();
+renderizaLista();
